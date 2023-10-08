4098c7d71b5f8797f3c7002b2a487665fad126d1
Miyuki <87884079+miyuki-KK@users.noreply.github.com>
2023-10-08 11:20:08 +0900

    Initial Commit

---
+++ toDoListNew/toDoListNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListNewApp.swift
+//  toDoListNew
+//
+//  Created by Miyuki Kikuchi on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct toDoListNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
